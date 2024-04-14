9627327a570474049da0347b735fca1a8e1e20a6
Anushtha <kumarsen.bash@gmail.com>
2024-04-14 21:09:04 +0530

    db connection

--- db.js
+++ db.js
@@ -2,11 +2,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-	user: "postgres",
-	host: "localhost",
-	database: "shaadi",
-	password: "root",
-	port: 5432,
+	ssl: "no-verify",
 });
 
 export default pool;
